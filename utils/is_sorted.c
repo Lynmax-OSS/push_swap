9038e48e49099c054df2c194518c5e2a9b3bc634
Keith <keith.teo2005@gmail.com>
2025-05-05 17:01:00 +0800

    checks if the stack is already sorted

---
+++ utils/is_sorted.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_sorted.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keteo <keteo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 16:56:01 by keteo             #+#    #+#             */
+/*   Updated: 2025/05/05 16:56:01 by keteo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int is_sorted(t_stack *stack)
+{
+	t_node	*current;
+	t_node	*next;
+	
+	while (current && current->next)
+	{
+		if (current->value > next->value)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
