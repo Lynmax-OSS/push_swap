45c522163c890830596fd9d24fb27103b46e1878
Keith <keith.teo2005@gmail.com>
2025-05-07 14:25:31 +0800

    updated header with more prototypes

--- push_swap.h
+++ push_swap.h
@@ -16,17 +16,25 @@
 
 typedef struct s_node
 {
-    int             value;
-    int             index;
-    struct s_node   *next;
+	int             value;
+	int             index;
+	struct s_node   *next;
 }t_node;
 
 typedef struct s_stack
 {
-    t_node  *top;
-    int     size;
+	t_node  *top;
+	int     size;
 }t_stack;
 
 void    error(char *str);
+t_stack *stack_init(void);
+t_stack	*free_stack(t_stack *stack);
+int		stack_size(t_stack *stack);
+void	assign_indices(t_stack *stack);
+void	error(char *str);
+int		is_sorted(t_stack *stack);
+void	parse_stack(t_stack *a, char **argv);
+void	parse_args(t_stack *a, int argc, char **argv);
 
 #endif
