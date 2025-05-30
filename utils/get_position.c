7840761db30f25a885064cd99b7b589acd43a339
Keith <keith.teo2005@gmail.com>
2025-05-31 00:29:41 +0800

    helper function for the small sorter

---
+++ utils/get_position.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_position.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keteo <keteo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 00:10:28 by keteo             #+#    #+#             */
+/*   Updated: 2025/05/31 00:10:28 by keteo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	get_position(t_stack *stack, int index)
+{
+	int	pos;
+	t_stack	*temp;
+
+	pos = 0;
+	temp = stack;
+	while (temp)
+	{
+		if (temp->index == index)
+			break;
+		pos++;
+		temp = temp->next;
+	}
+	return (pos);
+}
---
+++ utils/rotate_to_top.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_to_top.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keteo <keteo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 00:17:53 by keteo             #+#    #+#             */
+/*   Updated: 2025/05/31 00:17:53 by keteo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	rotate_to_top(t_stack **stack, int pos, char name)
+{
+	int	size;
+
+	size = stack_size(*stack);
+	if (pos <= size / 2)
+	{
+		while (pos-- > 0)
+		{
+			if (name == 'a')
+				ra(stack);
+			else
+				rb(stack);
+		}
+	}
+	else
+	{
+		pos = size - pos;
+		while (pos-- > 0)
+		{
+			if (name == 'a')
+				rra(stack);
+			else
+				rrb(stack);
+		}
+	}
+}
